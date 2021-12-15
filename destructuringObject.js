9354df95b6db506b2dc3b31ca571a2355d2dbc91
papo-97 <95270166+papo-97@users.noreply.github.com>
2021-12-15 14:26:12 -0800

    desctructuring objrct

---
+++ destructuringObject.js
@@ -0,0 +1,6 @@
+let person = {name: "Sarah", country:"Armenia", job:"Developer"};
+let name,country, job;
+const {name,country="Nigeria",job}=person;
+console.log(name);
+console.log(country);
+console.log(job);
